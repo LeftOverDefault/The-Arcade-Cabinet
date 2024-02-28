04f14cf1c89f2f7cd43fedf788b7d1355202443f
Michael Goddard <michaelgoddard@outlook.com>
2024-02-28 23:03:32 +0000

    added chunk rendering (broken)

--- arcade/func/generate_chunk.py
+++ arcade/func/generate_chunk.py
@@ -1,12 +1,12 @@
 from arcade.utils.imports import *
 
 
-def generate_chunk(x, y):
+def generate_chunk(config, x, y):
     chunk_data = []
-    for y_position in range(config["chunk_size"]):
-        for x_position in range(config["chunk_size"]):
-            target_x = x * config["chunk_size"] + x_position
-            target_y = y * config["chunk_size"] + y_position
+    for y_position in range(config.chunk_size):
+        for x_position in range(config.chunk_size):
+            target_x = x * config.chunk_size + x_position
+            target_y = y * config.chunk_size + y_position
             # tile_type = 0 # nothing
             # if target_y > 10:
             #     tile_type = 2 # dirt
--- arcade_test.py
+++ arcade_test.py
@@ -1,11 +1,11 @@
 import pygame
 import arcade
 from arcade.classes.camera import Camera
-from arcade.classes.configure import Configure
 from arcade.classes.layer import Layer
 from arcade.classes.player import Player
 from arcade.classes.tile.tile import Tile
 from arcade.debug.debugger import Debugger
+from arcade.func.generate_chunk import generate_chunk
 
 
 # world = {
@@ -46,6 +46,20 @@ config = {
 }
 
 
+def render_chunk(config, display_surface: pygame.Surface, chunk, camera, chunk_x_offset, chunk_y_offset):
+    tiles = []
+    for location in chunk:
+        x = location[0]
+        y = location[1]
+        sprite = pygame.sprite.Sprite()
+        sprite.image = pygame.Surface((config.tile_size, config.tile_size))
+        sprite.image.fill((0, 255, 255))
+        sprite.rect = sprite.image.get_rect(topleft=(x * config.tile_size + chunk_x_offset, y * config.tile_size + chunk_y_offset, config.tile_size, config.tile_size))
+        tiles.append(sprite)
+    return tiles
+
+
+
 class Main:
     def __init__(self) -> None:
         self.layer_1 = Layer(y_sorted=True, collidable=False)
@@ -53,23 +67,20 @@ class Main:
         self.debugger = Debugger(font_path="./assets/font/font.png", config=self.window.config)
 
         self.camera = Camera(display_surface=self.window.display_surface)
-        self.player = Player(initial_position=(0, 0), path="./assets/sprite/entity/player/", group=self.layer_1, config=self.window.config)
+        self.player = Player(initial_position=(-12, -12), path="./assets/sprite/entity/player/", group=self.layer_1, config=self.window.config)
 
         self.window.render = self.render
         self.window.update = self.update
         self.window.event = self.event
         self.debugger.render = self.debug_render
 
-
         self.camera.camera_delay = 25
 
-        self.layer_2 = Layer(y_sorted=True, collidable=True)
-
-        tile = Tile(position=(0, 0), group=self.layer_2, config=self.window.config)
+        self.visible_chunks = [(self.camera.offset.x // self.window.config.chunk_size, self.camera.offset.y // self.window.config.chunk_size)]
 
         self.camera.add_group(group=self.layer_1, player=self.player)
-        self.camera.add_group(group=self.layer_2, player=self.player)
 
+        self.layer = Layer(y_sorted=False, collidable=False)
 
         # pygame.mouse.set_visible(False)
         # self.mouse_pos = pygame.Vector2()
@@ -78,12 +89,29 @@ class Main:
 
     def render(self):
         self.window.display_surface.fill((255, 255, 255))
+
+        for item in self.visible_chunks:
+            chunk_x = int(item[0])
+            chunk_y = int(item[1])
+            chunk = generate_chunk(config=self.window.config, x=chunk_x, y=chunk_y)
+
+            # tiles = render_chunk(self.window.config, self.window.display_surface, self.camera, chunk, chunk_x * self.window.config.chunk_size * self.window.config.tile_size - self.camera.offset.x * self.window.config.tile_size, chunk_y * self.window.config.chunk_size * self.window.config.tile_size - self.camera.offset.y * self.window.config.tile_size)
+
+            # for sprite in tiles:
+                # self.window.display_surface.blit(source=sprite.image, dest=sprite.rect)
+
+            # self.layer.add(tile for tile in tiles)
+
+            # self.camera.add_group(self.layer, self.player)
+
+
+            # render_chunk(self.window.config, self.window.display_surface, chunk, (chunk_x * self.window.config.tile_size) - self.camera.offset.x * self.window.config.tile_size, (chunk_y * self.window.config.tile_size) - self.camera.offset.y * self.window.config.tile_size)
+
         self.camera.draw(player=self.player)
         self.window.screen.blit(source=arcade.pygame.transform.scale(surface=self.window.display_surface, size=self.window.screen.get_size()), dest=(0, 0))
         self.debugger.draw()
-
         # self.window.screen.blit(self.mouse_img, self.mouse_pos)
-    
+
 
     def debug_render(self):
         self.debugger.font.render(surface=self.debugger.surface, text=f"FPS: {round(number=self.window.clock.get_fps())}", location=(10, 10))
@@ -96,8 +124,8 @@ class Main:
         self.camera.update(delta_time=self.window.delta_time)
         self.mouse_pos = pygame.mouse.get_pos()
 
-        pygame.event.set_grab(True)
-    
+        pygame.event.set_grab(False)
+
 
     def event(self, event):
         if event.type == pygame.KEYDOWN:
